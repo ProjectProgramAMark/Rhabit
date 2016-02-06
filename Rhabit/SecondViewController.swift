7ad2ce5f5c21bd6edfe844a85928d0083cfdb962
ProjectProgramAMark <projectprogramamark@gmail.com>
2016-02-06 14:26:39 -0500

    Initial 3756268735th commit

---
+++ Rhabit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Rhabit
+//
+//  Created by Mark Moussa on 2/6/16.
+//  Copyright © 2016 Mark Moussa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
