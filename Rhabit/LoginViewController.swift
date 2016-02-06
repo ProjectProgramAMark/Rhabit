7e3987f45c149f44d458c1b607dad7e217563023
Jpb14d <Johnnyourking@gmail.com>
2016-02-06 18:18:23 -0500

    Button working UI

--- Rhabit/LoginViewController.swift
+++ Rhabit/LoginViewController.swift
@@ -7,10 +7,39 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var loginButton: UISegmentedControl!
-    @IBOutlet weak var registerButton: UISegmentedControl!
-    @IBOutlet weak var submitButton: UIButton!
+    
     var myRootRef = Firebase(url:"https://scorching-torch-468.firebaseio.com/")
+    
+    @IBAction func optionSegment(sender: UISegmentedControl) {
+        if(sender.selectedSegmentIndex == 0) {
+            myRootRef.authUser(self.usernameField.text, password: self.passwordField.text,
+                withCompletionBlock: { error, authData in
+                    if error != nil {
+                        // There was an error logging in to this account
+                    } else {
+                        // We are now logged in
+                    }
+                })
+            } else if(sender.selectedSegmentIndex == 1) {
+                myRootRef.createUser(self.usernameField.text, password: self.passwordField.text,
+                    withValueCompletionBlock: { error, result in
+                        if error != nil {
+                            // There was an error creating the account
+                        } else {
+                            let uid = result["uid"] as? String
+                            print("Successfully created user account with uid: \(uid)")
+                        }
+                })
+            }
+        }
+    
+    @IBAction func submitButton(sender: AnyObject) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let tabBarController = storyboard.instantiateViewControllerWithIdentifier("TabBarController") as! UITabBarController
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        appDelegate.window?.rootViewController = tabBarController
+    }
+    
 
     @IBAction func submit(sender: UISegmentedControl) {
         if(sender.selectedSegmentIndex == 0) {
@@ -34,7 +63,6 @@ class LoginViewController: UIViewController {
             })
         }
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
